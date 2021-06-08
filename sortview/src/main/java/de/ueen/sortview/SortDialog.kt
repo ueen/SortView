5873a0b9ce97b9063d9aa428ce4a4237233d3569
ueen <ueli.sarnighausen@online.de>
2021-06-08 11:53:38 +0200

    added horizontal option in SortDialog

--- sortview/src/main/java/de/ueen/sortview/SortDialog.kt
+++ sortview/src/main/java/de/ueen/sortview/SortDialog.kt
@@ -2,6 +2,7 @@ package de.ueen.sortview
 
 import android.R
 import android.content.Context
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -10,7 +11,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 class SortDialog() {
 
     companion object {
-        fun <ITEM> show(context: Context, list: List<ITEM>, title: String? = null, itemLayout: Int = R.layout.simple_list_item_1, listener: (List<ITEM>) -> Unit) {
+        fun <ITEM> show(context: Context, list: List<ITEM>, title: String? = null, horizontal: Boolean = false, itemLayout: Int = R.layout.simple_list_item_1, listener: (List<ITEM>) -> Unit) {
             val builder: AlertDialog.Builder = AlertDialog.Builder(context)
             builder.setTitle(title)
 
@@ -18,12 +19,16 @@ class SortDialog() {
                 .setupAdapter { adapter ->
                     adapter.setItemLayout(itemLayout)
                     adapter.setLayoutManager(
-                        LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false))
+                        LinearLayoutManager(context, if (horizontal) LinearLayoutManager.HORIZONTAL else LinearLayoutManager.VERTICAL, false))
                     adapter.onBind { view, s ->
-                        view.findViewById<TextView>(R.id.text1).text = s }
+                        view.findViewById<TextView>(R.id.text1).text = s
+                        if (horizontal) view.layoutParams.width = LinearLayout.LayoutParams.WRAP_CONTENT
+                    }
                     adapter.setItems(list.map { i -> i.toString() })
                 }
                 .onSort(list)
+
+            if (horizontal) sortView.equalSpacing(true, true)
 
             builder.setView(sortView)
 
